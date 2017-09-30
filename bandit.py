c0d3a7077e539cb5d17e026aa473aec20644a5b8
SahilC <sahilc.2200@gmail.com>
2017-09-30 23:23:38 +0530

    Commiting code

--- bandit.py
+++ bandit.py
@@ -5,7 +5,10 @@ class Bandit:
         self.k = k
         
         # qstar: action values
-        self.qstar = np.random.normal(size=k)
+        # self.qstar = []
+        # for i in xrange(1, k+1):
+        # 	v = np.random.normal(5 + i, 1)
+        # 	self.qstar.append(v)
     
     def action(self, a):
-        return np.random.normal(loc=self.qstar[a])
+        return np.random.normal(5 + a, 1)
--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 from greedy_bandit import *
 
 if __name__ == '__main__':
+	# 10 arms 
 	k = 10
 	numsteps = 1000
-	numtasks = 2000
-
+	numtasks = 200
+	epislon = [0, 0.01, 0.1]
 	avgR = np.zeros((numsteps, ))
-	for task in range(2000):
-	    bandit_task = greedy_action_selection(k,numsteps)
-	    avgR += bandit_task['R']
+	for e in epislon:
+		for task in range(2000):
+		    bandit_task = greedy_action_selection(k, numsteps, e)
+		    avgR += bandit_task['R']
 	avgR /= numtasks
 
 	plt.plot(avgR)
