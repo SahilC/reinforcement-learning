40514c1e5d80133a28a5e7145c44d3548e7f9fc0
SahilC <sahilc.2200@gmail.com>
2017-09-30 23:42:35 +0530

    Removing prints

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import numpy as np
-
-import matplotlib.pyplot as plt
-
 from greedy_bandit import *
+
+# import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 	# 10 arms 
@@ -14,18 +13,17 @@ if __name__ == '__main__':
 	
 	for e in epislon:
 		print 'Starting'
+
 		for task in range(numtasks):
 		    bandit_task = greedy_action_selection(k, numsteps, e)
 		    avgR += bandit_task['R']
+		avgR /= numtasks
 
 		file_name = 'results_'+str(e)+'.npy'
-		avgR /= numtasks
 		np.save(file_name, avgR)
 		
-
-
-	plt.plot(avgR)
-	plt.ylabel('Average reward')
-	plt.xlabel('Steps')
-	plt.xlim(-5)
-	plt.show()
+	# plt.plot(avgR)
+	# plt.ylabel('Average reward')
+	# plt.xlabel('Steps')
+	# plt.xlim(-5)
+	# plt.show()
